55b145d44657a583ab71fba1066fab1f15592e00
Neelansh Mathur <53081208+neelansh15@users.noreply.github.com>
2024-12-07 02:20:45 +0530

    feat: mint functions

--- contracts/UserItems.sol
+++ contracts/UserItems.sol
@@ -2,8 +2,9 @@
 pragma solidity ^0.8.20;
 
 import {ERC1155} from "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
-contract UserItems is ERC1155 {
+contract UserItems is ERC1155, Ownable {
     string public constant name = "UserItems";
     uint256 public constant CASH = 0; // ERC20 with 18 decimals
 
@@ -11,7 +12,10 @@ contract UserItems is ERC1155 {
 
     mapping(address => bool) hasClaimedStarterPack;
 
-    constructor() ERC1155("https://localhost:8001/items/{id}.json") {
+    constructor()
+        ERC1155("https://localhost:8001/items/{id}.json")
+        Ownable(msg.sender)
+    {
         _mint(msg.sender, CASH, 10 ** 18, "");
     }
 
@@ -39,5 +43,44 @@ contract UserItems is ERC1155 {
         );
 
         super.safeTransferFrom(from, to, id, amount, data);
+    }
+
+    function safeBatchTransferFrom(
+        address from,
+        address to,
+        uint256[] memory ids,
+        uint256[] memory amounts,
+        bytes memory data
+    ) public override {
+        for (uint256 i = 0; i < ids.length; ) {
+            require(
+                ids[i] != CASH,
+                "Cash cannot be transferred, only minted or burned"
+            );
+
+            unchecked {
+                i++;
+            }
+        }
+
+        super.safeBatchTransferFrom(from, to, ids, amounts, data);
+    }
+
+    function mint(
+        address account,
+        uint256 id,
+        uint256 amount,
+        bytes memory data
+    ) public onlyOwner {
+        _mint(account, id, amount, data);
+    }
+
+    function mintBatch(
+        address account,
+        uint256[] memory ids,
+        uint256[] memory amounts,
+        bytes memory data
+    ) public onlyOwner {
+        _mintBatch(account, ids, amounts, data);
     }
 }
