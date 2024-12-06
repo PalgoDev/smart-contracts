6536e3b5c2d81e8ee4d42b1d8bc3847b49b34446
Neelansh Mathur <53081208+neelansh15@users.noreply.github.com>
2024-12-07 00:58:39 +0530

    feat: deploy to polygon network

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,17 @@
 import type { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox-viem";
+import { vars } from "hardhat/config";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
+  
+  // Reference: https://hardhat.org/tutorial/deploying-to-a-live-network#_7-deploying-to-a-live-network
+  networks: {
+    polygon: {
+      accounts: [vars.get("DEFAULT_PRIVATE_KEY")],
+      url: vars.get("POLYGON_RPC_URL"),
+    },
+  },
 };
 
 export default config;
