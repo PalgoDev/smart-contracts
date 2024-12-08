a75175ba745bf640ad5de7ed981b3e3302339c1c
Neelansh Mathur <53081208+neelansh15@users.noreply.github.com>
2024-12-08 07:26:01 +0530

    chore: add more networks to hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,18 +4,30 @@ import { vars } from "hardhat/config";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
-  
+
   // Reference: https://hardhat.org/tutorial/deploying-to-a-live-network#_7-deploying-to-a-live-network
   networks: {
     polygon: {
-      accounts: [vars.get("DEFAULT_PRIVATE_KEY")],
       url: vars.get("POLYGON_RPC_URL"),
+      accounts: [vars.get("DEFAULT_PRIVATE_KEY")],
+    },
+    base: {
+      url: vars.get("BASE_RPC_URL"),
+      accounts: [vars.get("DEFAULT_PRIVATE_KEY")],
+    },
+    bsc: {
+      url: vars.get("BSC_RPC_URL"),
+      accounts: [vars.get("DEFAULT_PRIVATE_KEY")],
     },
   },
 
   etherscan: {
-    apiKey: vars.get("POLYGONSCAN_API_KEY"),
-  }
+    apiKey: {
+      polygon: vars.get("POLYGONSCAN_API_KEY"),
+      bsc: vars.get("BSCSCAN_API_KEY"),
+      base: vars.get("BASESCAN_API_KEY"),
+    },
+  },
 };
 
 export default config;
